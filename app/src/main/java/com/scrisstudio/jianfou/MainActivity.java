ff0e8b0d313b4ad1e72adb5387795723c7626f47
r-q <scrisqiu@hotmail.com>
2021-02-11 14:12:22 +0800

    add splash

--- app/src/main/java/com/scrisstudio/jianfou/MainActivity.java
+++ app/src/main/java/com/scrisstudio/jianfou/MainActivity.java
@@ -2,9 +2,20 @@ package com.scrisstudio.jianfou;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+	@Override
+	public void onBackPressed() {
+		// super.onBackPressed();
+		Intent intent = new Intent(Intent.ACTION_MAIN);
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		intent.addCategory(Intent.CATEGORY_HOME);
+		startActivity(intent);
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/scrisstudio/jianfou/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.scrisstudio.jianfou;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class SplashActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash_activity);
+
+		Handler handler = new Handler(Looper.getMainLooper());
+		handler.postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+				startActivity(intent);
+				finish();
+			}
+		}, 1500);
+
+	}
+}
