df1978aa40d7983649a20a889ca7d390e6f015be
r-q <scrisqiu@hotmail.com>
2021-02-14 23:04:48 +0800

    make homepage basic skeleton

--- app/src/main/java/com/scrisstudio/jianfou/MainActivity.java
+++ app/src/main/java/com/scrisstudio/jianfou/MainActivity.java
@@ -1,12 +1,18 @@
 package com.scrisstudio.jianfou;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.scrisstudio.jianfou.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+	private static final String TAG = "Jianfou";
+	ActivityMainBinding binding;
 
 	@Override
 	public void onBackPressed() {
@@ -20,6 +26,39 @@ public class MainActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		binding = ActivityMainBinding.inflate(getLayoutInflater());
+		View view = binding.getRoot();
+		setContentView(view);
+
+		binding.topAppBar.setOnMenuItemClickListener(menuItem -> {
+			switch (menuItem.getItemId()) {
+				case R.id.help:
+					Toast.makeText(this.getApplicationContext(), "还没有完成。", Toast.LENGTH_LONG).show();
+					return true;
+				default:
+					return false;
+			}
+		});
+
+		binding.topAppBar.setNavigationOnClickListener(v -> binding.drawerLayout.openDrawer(binding.navigation));
+
+		binding.navigation.setNavigationItemSelectedListener(menuItem -> {
+			for (int i = 0; i < binding.navigation.getMenu().size(); i++)
+				binding.navigation.getMenu().getItem(i).setChecked(false);
+
+			menuItem.setChecked(true);
+			binding.drawerLayout.closeDrawer(binding.navigation);
+
+			switch (menuItem.getItemId()) {
+				default:
+					Toast.makeText(this.getApplicationContext(), "还没有完成。", Toast.LENGTH_LONG).show();
+					return false;
+			}
+		});
+
+		binding.floatingActionButton.setOnClickListener(v -> {
+			Toast.makeText(this.getApplicationContext(), "还没有完成。", Toast.LENGTH_LONG).show();
+		});
+
 	}
 }
--- app/src/main/java/com/scrisstudio/jianfou/SplashActivity.java
+++ app/src/main/java/com/scrisstudio/jianfou/SplashActivity.java
@@ -22,7 +22,7 @@ public class SplashActivity extends AppCompatActivity {
 				startActivity(intent);
 				finish();
 			}
-		}, 1500);
+		}, 500);
 
 	}
 }
